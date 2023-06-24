68665b174793d0e4bc3bdc016734db09224c0924
«Pavel» <paveligorevich2108@gmail.com>
2023-06-24 18:58:39 +0200

    Lesson 1 Task 3

---
+++ lesson_1/1_3/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = int.Parse(Console.ReadLine()!); // a = 4
+int count = -a; // count = -4
+Console.WriteLine(count); 
+while (count != a){
+    if (a>0)
+count++;// count = count + 1
+else
+count--;
+Console.WriteLine(count);
+}
