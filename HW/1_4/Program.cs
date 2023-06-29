7334b68fa8db253d5fadae4fb5a74fcf262a51f5
«Pavel» <paveligorevich2108@gmail.com>
2023-06-29 15:29:18 +0200

    HW tusk 1_4

---
+++ HW/1_4/Program.cs
@@ -0,0 +1,12 @@
+﻿{
+Console.WriteLine("Введите число:");
+        int number = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Четные числа от 1 до {0}:", number);
+        for (int i = 1; i <= number; i++)
+        {
+            if (i % 2 == 0)
+            
+                Console.WriteLine(i);
+        }
+}
