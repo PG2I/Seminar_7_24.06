5d415bdb4c50c36bbd2b54223e48538cf17ddc20
«Pavel» <paveligorevich2108@gmail.com>
2023-06-30 13:29:23 +0200

    HW tusk 2_1

---
+++ HW/2_1/Program.cs
@@ -0,0 +1,18 @@
+﻿ //Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число:");
+    int number = int.Parse(Console.ReadLine()!);
+
+    int secondDigit;
+
+    if (number >= 100 && number <= 999)
+     {
+         secondDigit = ((number / 10) % 10);
+    }
+    else
+    {
+        Console.WriteLine("Введено некорректное число!");
+        return;
+    }
+
+    Console.WriteLine("Вторая цифра числа: " + secondDigit);
