7effccb73f415100a53691c72adc85e61d017dec
«Pavel» <paveligorevich2108@gmail.com>
2023-07-08 16:54:37 +0200

    HW Tusk 4_3

---
+++ HW/4_3/Program.cs
@@ -0,0 +1,35 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.WriteLine("Enter size:");
+int Size = int.Parse(Console.ReadLine()!);
+int[] array = new int[Size];
+Console.WriteLine("Enter from:");
+int from = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter to:");
+int to = int.Parse(Console.ReadLine()!);
+
+
+void FillArray(int[] Size)
+{
+int length = Size.Length;
+int index = 0;
+while (index < length)
+{
+Size[index] = new Random().Next(from, to);
+index++;
+}
+}
+
+void PrintArray(int[] col)
+{
+int count = col.Length;
+int position = 0;
+while (position < count)
+{
+System.Console.Write(col[position] + " ");
+position++;
+}
+}
+
+FillArray(array);
+PrintArray(array);
