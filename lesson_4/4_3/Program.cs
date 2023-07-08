1bd0930abd7b1a76dfa58fa65158f8b5b8c7fbed
«Pavel» <paveligorevich2108@gmail.com>
2023-07-08 14:10:48 +0200

    Lesson 4

---
+++ lesson_4/4_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// 2. Напишите программу, которая принимает на вход
+// число N и выдаёт произведение чисел от 1 до N.
+
+Console.WriteLine("Введите число");
+
+int SumNum(int num)
+{
+    int all_sum = 1;
+    for (int i = 1; i <= num; i++)
+    all_sum *= i;
+
+    return all_sum;
+}
+
+int A = int.Parse(Console.ReadLine()!);
+int result = SumNum(A);
+Console.WriteLine(result);
