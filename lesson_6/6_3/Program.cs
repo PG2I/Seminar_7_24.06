59c04f060ad0c038c749133a1188c50b04a7acf6
«Pavel» <paveligorevich2108@gmail.com>
2023-07-12 22:45:40 +0200

    lesson 6 all

---
+++ lesson_6/6_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// 2. Напишите программу, которая будет
+// преобразовывать десятичное число в двоичное.
+
+ Console.WriteLine("Введите десятичное число:");
+        int decimalNumber = int.Parse(Console.ReadLine()!);
+
+        string binaryNumber = DecimalToBinary(decimalNumber);
+        
+        Console.WriteLine($"Бинарное: {binaryNumber}");
+    
+
+string DecimalToBinary(int decimalNumber)
+    {
+        string binary = "";
+        
+        for (int i = decimalNumber; i > 0; i /= 2)
+        {
+            int remainder = i % 2;
+            binary = remainder + binary;
+        }
+
+        return binary;
+    }
---
+++ lesson_6/6_4/Program.cs
@@ -0,0 +1,31 @@
+﻿// 3. Не используя рекурсию, выведите первые N чисел Фибоначчи.
+// Первые два числа Фибоначчи: 0 и 1.
+
+ Console.WriteLine("Количество чисел:");
+        int n = int.Parse(Console.ReadLine()!);
+
+    static void PrintFibonacciNumbers(int n)
+    {
+        if (n < 1)
+        {
+            Console.WriteLine("Неверно введено число.");
+            return;
+        }
+
+        int first = 0;
+        int second = 1;
+        
+        Console.Write("Число Фибоначчи:");
+        Console.Write($" {first} {second}");
+
+        for (int i = 2; i < n; i++)
+        {
+            int next = first + second;
+            Console.Write($" {next}");
+            first = second;
+            second = next;
+        }
+
+    }
+
+     PrintFibonacciNumbers(n);
