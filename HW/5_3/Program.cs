fd8a202886718a91e769f035512836aa63e82570
«Pavel» <paveligorevich2108@gmail.com>
2023-07-12 23:26:58 +0200

    HW Tusk 5_3

---
+++ HW/5_3/Program.cs
@@ -0,0 +1,60 @@
+﻿
+
+   
+Console.WriteLine("Size array: ");
+int size = int.Parse(Console.ReadLine()!);
+Console.WriteLine("from:");
+int from = int.Parse(Console.ReadLine()!);
+Console.WriteLine("to:");
+int to = int.Parse(Console.ReadLine()!);
+
+    static void Print(double[] arr)
+    {
+        int size = arr.Length;
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write($"{arr[i]} ");
+        }
+        Console.WriteLine();
+    }
+
+    static void FillArray(double[] arr, double from, double to)
+    {
+        int length = arr.Length;
+        int index = 0;
+        Random random = new Random();
+        while (index < length)
+        {
+            double randomNumber = random.NextDouble() * (to - from) + from;
+            arr[index] = Math.Round(randomNumber, 2);
+            index++;
+        }
+    }
+
+    static double FindDifference(double[] arr)
+    {
+        double min = arr[0];
+        double max = arr[0];
+        int size = arr.Length;
+        for (int i = 1; i < size; i++)
+        {
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+            else if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+        return Math.Round(max - min, 2);
+    }
+
+     double[] array = new double[size];
+        FillArray(array, from, to);
+
+        Console.WriteLine("Array:");
+        Print(array);
+
+        double difference = FindDifference(array);
+        Console.WriteLine($"Difference: {difference}");
