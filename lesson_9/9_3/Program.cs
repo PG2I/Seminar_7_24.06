e3e31a8bbade9192a2a000c62a18330bf7c3868d
«Pavel» <paveligorevich2108@gmail.com>
2023-07-20 20:45:05 +0200

    Lesson 9 all

---
+++ lesson_9/9_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// 3. Напишите программу, которая будет принимать на вход число
+//    и возвращать сумму его цифр. Использовать рекурсию.
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int PrintNumber(int num)
+{
+    if (num/10 == 0)  return num;
+
+   return PrintNumber(num/10) + num % 10;
+}
+PrintNumber(num);
+
+Console.Write($"{PrintNumber(num) }");
