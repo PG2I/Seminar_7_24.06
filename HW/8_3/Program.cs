f501c62cf8ddf31969b7f3a7b4de69da9e3e5484
«Pavel» <paveligorevich2108@gmail.com>
2023-07-24 14:47:45 +0200

    HW tusk 8_3, 8_4

---
+++ HW/8_3/Program.cs
@@ -0,0 +1,100 @@
+﻿//Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+
+Console.Write("Кол-во строк первой матрицы: ");
+int rows1 = int.Parse(Console.ReadLine()!);
+Console.Write("Кол-во столбцов первой матрицы: ");
+int cols1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Кол-во строк второй матрицы: ");
+int rows2 = int.Parse(Console.ReadLine()!);
+Console.Write("Кол-во столбцов второй матрицы: ");
+int cols2 = int.Parse(Console.ReadLine()!);
+
+if (cols1 != rows2)
+{
+    Console.WriteLine("Матрицы не совпадают");
+    return;
+}
+
+int[,] matrix1 = new int[rows1, cols1];
+int[,] matrix2 = new int[rows2, cols2];
+
+Console.Write("Минимальное значение: ");
+int minValue = int.Parse(Console.ReadLine()!);
+Console.Write("Максимальное значение: ");
+int maxValue = int.Parse(Console.ReadLine()!);
+
+FillArray(matrix1, minValue, maxValue);
+FillArray(matrix2, minValue, maxValue);
+
+Console.WriteLine("Первая матрица:");
+PrintArray(matrix1);
+
+Console.WriteLine("Вторая матрица:");
+PrintArray(matrix2);
+
+int[,] resultMatrix = MultiplyMatrices(matrix1, matrix2);
+
+if (resultMatrix != null)
+{
+    Console.WriteLine("Результирующая матрица:");
+    PrintArray(resultMatrix);
+}
+
+
+static void FillArray(int[,] matrix, int minValue, int maxValue)
+{
+    Random random = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(minValue, maxValue + 1);
+        }
+    }
+}
+
+static void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+static int[,] MultiplyMatrices(int[,] matrix1, int[,] matrix2)
+{
+    int rows1 = matrix1.GetLength(0);
+    int cols1 = matrix1.GetLength(1);
+    int rows2 = matrix2.GetLength(0);
+    int cols2 = matrix2.GetLength(1);
+
+    if (cols1 != rows2)
+    {
+        Console.WriteLine("Матрицы не совпадают");
+        return null;
+    }
+
+    int[,] resultMatrix = new int[rows1, cols2];
+
+    for (int i = 0; i < rows1; i++)
+    {
+        for (int j = 0; j < cols2; j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < cols1; k++)
+            {
+                sum += matrix1[i, k] * matrix2[k, j];
+            }
+            resultMatrix[i, j] = sum;
+        }
+    }
+
+    return resultMatrix;
+}
---
+++ HW/8_4/Program.cs
@@ -0,0 +1,100 @@
+﻿// Задайте двумерный массив из целых чисел. 
+//Напишите программу, которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+
+
+Console.Write("Кол-во строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Кол-во столбцов: ");
+int cols = int.Parse(Console.ReadLine()!);
+
+int[,] matrix = new int[rows, cols];
+
+Console.Write("Минимальное значение: ");
+int minValue = int.Parse(Console.ReadLine()!);
+Console.Write("Максимальное значение: ");
+int maxValue = int.Parse(Console.ReadLine()!);
+
+FillArray(matrix, minValue, maxValue);
+
+Console.WriteLine("Оригинальный массив:");
+PrintArray(matrix);
+
+int minElement = int.MaxValue;
+int minRow = -1;
+int minCol = -1;
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        if (matrix[i, j] < minElement)
+        {
+            minElement = matrix[i, j];
+            minRow = i;
+            minCol = j;
+        }
+    }
+}
+
+if (minRow >= 0 && minCol >= 0)
+{
+    int[,] resultMatrix = RemoveRowAndColumn(matrix, minRow, minCol);
+
+    Console.WriteLine("Массив без наименьшего элемента:");
+    PrintArray(resultMatrix);
+}
+else
+{
+    Console.WriteLine("Массив пуст или не содержит наименьшего элемента.");
+}
+
+int[,] RemoveRowAndColumn(int[,] matrix, int row, int col)
+{
+    int rows = matrix.GetLength(0);
+    int cols = matrix.GetLength(1);
+
+    int[,] resultMatrix = new int[rows - 1, cols - 1];
+
+    for (int i = 0, k = 0; i < rows; i++)
+    {
+        if (i == row) continue;
+
+        for (int j = 0, l = 0; j < cols; j++)
+        {
+            if (j == col) continue;
+
+            resultMatrix[k, l] = matrix[i, j];
+            l++;
+        }
+
+        k++;
+    }
+
+    return resultMatrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matrix, int minValue, int maxValue)
+{
+    Random random = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(minValue, maxValue + 1);
+        }
+    }
+}
+
