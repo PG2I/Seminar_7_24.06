e61e928157ba6023985388ee7126c716fcdd7b44
«Pavel» <paveligorevich2108@gmail.com>
2023-07-24 18:27:49 +0200

    Seminar 7 All lesson and All HW

---
+++ HW/9_2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму
+//натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+Console.WriteLine("Введите M:");
+int M = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите N:");
+int N = int.Parse(Console.ReadLine()!);
+
+int sum = SumNumbers(M, N);
+
+Console.WriteLine($"Сумма натуральных элементов от {M} до {N}: {sum}");
+
+int SumNumbers(int current, int N)
+{
+    if (current > N)
+    {
+        return 0; 
+    }
+
+    return current + SumNumbers(current + 1, N); // Суммируем текущее значение с результатом рекурсивного вызова функции для следующего числа.
+}
+
+SumNumbers(M, N);
